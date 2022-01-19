01ed23a09c534aaa8a630174323a9a3d801b4ddb
Kishan Jaiswal <jaiswalkishan09@gmail.com>
2022-01-19 20:52:27 +0545

    pointer is shifted to icon

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -78,12 +78,9 @@ export default function NavBar() {
               />
             </div>
           </div>
-          <div
-            className="col-6"
-            style={{ textAlign: "right" }}
-            onClick={() => window.location.reload(false)}
-          >
+          <div className="col-6" style={{ textAlign: "right" }}>
             <img
+              onClick={() => window.location.reload(false)}
               src={home}
               style={{ height: "16px", width: "16px", cursor: "pointer" }}
             ></img>
